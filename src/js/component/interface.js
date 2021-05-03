436bbe8f3d940e26c591455500a2b6e8b2c99d78
Cyberia-Place <eliasberto2@gmail.com>
2021-05-03 13:09:26 +0000

    avanzando con el diseño

---
+++ src/js/component/interface.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export const Interface = () => {
+	return (
+		<div>
+			<h2>It is ? Turn!</h2>
+			<div>
+				<div className="row no-gutters">
+					<div className="col-1">
+						<div className="block border-bottom border-right"></div>
+					</div>
+					<div className="col-1">
+						<div className="block border-bottom border-right"></div>
+					</div>
+					<div className="col-1">
+						<div className="block border-bottom border-right"></div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-2">
+						<div className="block border-bottom border-right"></div>
+					</div>
+					<div className="col-2">
+						<div className="block border-bottom border-right"></div>
+					</div>
+					<div className="col-2">
+						<div className="block border-bottom border-right"></div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-2">
+						<div className="block border-bottom border-right"></div>
+					</div>
+					<div className="col-2">
+						<div className="block border-bottom border-right"></div>
+					</div>
+					<div className="col-2">
+						<div className="block border-bottom border-right"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
