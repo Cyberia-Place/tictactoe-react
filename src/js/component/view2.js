3c57d6dbe588eb66985b8754b0e786a5571c34d5
Cyberia-Place <eliasberto2@gmail.com>
2021-05-05 17:56:05 +0000

    casi

--- src/js/component/view2.js
+++ src/js/component/view2.js
@@ -10,14 +10,19 @@ export const View2 = props => {
 		if (props.player == "X") {
 			let total_turns = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
 			setTurn(total_turns[i]);
+			console.log(turn);
 			i++;
-			total_casillas[casilla] = `dangerouslySetInnerHTML=${{ __html: turn}}`;
-			console.log(total_casillas[casilla]);
+			total_casillas[casilla] = `dangerouslySetInnerHTML=${{
+				__html: turn
+			}}`;
+			console.log(total_casillas);
 		} else {
 			let total_turns = ["O", "X", "O", "X", "O", "X", "O", "X", "O"];
 			setTurn(total_turns[i]);
 			i++;
-			total_casillas[casilla] = `dangerouslySetInnerHTML=${{ __html: turn}}`;
+			total_casillas[casilla] = `dangerouslySetInnerHTML=${{
+				__html: turn
+			}}`;
 		}
 	};
 
@@ -28,50 +33,44 @@ export const View2 = props => {
 				<div className="row justify-content-center">
 					<div
 						className="block border-bottom border-right border-bottom-color border-right-color"
-						{total_casillas[0]}
+						{...total_casillas[0]}
 						onClick={() => putMark("0")}></div>
 					<div
 						className="block border-bottom border-bottom-color"
-						onClick={() => putMark("1")}>
-						{total_casillas[1]}
-					</div>
+						{...total_casillas[1]}
+						onClick={() => putMark("1")}></div>
 					<div
 						className="block border-bottom border-left border-bottom-color border-left-color"
-						onClick={() => putMark("2")}>
-						{total_casillas[2]}
-					</div>
+						{...total_casillas[2]}
+						onClick={() => putMark("2")}></div>
 				</div>
 				<div className="row justify-content-center">
 					<div
 						className="block border-right border-right-color"
-						onClick={() => putMark("3")}>
-						{total_casillas[3]}
-					</div>
-					<div className="block" onClick={() => putMark("4")}>
-						{total_casillas[4]}
-					</div>
+						{...total_casillas[3]}
+						onClick={() => putMark("3")}></div>
+					<div
+						className="block"
+						{...total_casillas[4]}
+						onClick={() => putMark("4")}></div>
 					<div
 						className="block border-left border-left-color"
-						onClick={() => putMark("5")}>
-						{total_casillas[5]}
-					</div>
+						{...total_casillas[5]}
+						onClick={() => putMark("5")}></div>
 				</div>
 				<div className="row justify-content-center">
 					<div
 						className="block border-top border-right border-top-color border-right-color"
-						onClick={() => putMark("6")}>
-						{total_casillas[6]}
-					</div>
+						{...total_casillas[6]}
+						onClick={() => putMark("6")}></div>
 					<div
 						className="block border-top border-top-color"
-						onClick={() => putMark("7")}>
-						{total_casillas[7]}
-					</div>
+						{...total_casillas[7]}
+						onClick={() => putMark("7")}></div>
 					<div
 						className="block border-top border-left border-top-color border-left-color"
-						onClick={() => putMark("8")}>
-						{total_casillas[8]}
-					</div>
+						{...total_casillas[8]}
+						onClick={() => putMark("8")}></div>
 				</div>
 			</div>
 		</div>
