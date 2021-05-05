f3b84b88915651bdbc4fd6cc4e2c4347ea2e5862
Cyberia-Place <eliasberto2@gmail.com>
2021-05-05 16:05:28 +0000

    casi

--- src/js/component/view2.js
+++ src/js/component/view2.js
@@ -1,68 +1,77 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
+let i = 0;
 export const View2 = props => {
-	let i = 0;
-	const [turn, setTurn] = useState({ __html: props.player });
-
-	const putMark = () => {
-		console.log(props.player);
+	const [turn, setTurn] = useState(props.player);
+	let total_casillas = ["", "", "", "", "", "", "", "", ""];
+	// console.log(i);
+	const putMark = casilla => {
 		if (props.player == "X") {
 			let total_turns = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
-			setTurn({ __html: total_turns[i] });
+			setTurn(total_turns[i]);
 			i++;
+			total_casillas[casilla] = `dangerouslySetInnerHTML=${{ __html: turn}}`;
+			console.log(total_casillas[casilla]);
 		} else {
 			let total_turns = ["O", "X", "O", "X", "O", "X", "O", "X", "O"];
-			setTurn({ __html: total_turns[i] });
+			setTurn(total_turns[i]);
 			i++;
+			total_casillas[casilla] = `dangerouslySetInnerHTML=${{ __html: turn}}`;
 		}
 	};
 
 	return (
 		<div className="container">
-			<h2>It is {Object.values(turn)[0]} Turn!</h2>
+			<h2>It is {turn} Turn!</h2>
 			<div className="mx-auto mt-4">
 				<div className="row justify-content-center">
 					<div
 						className="block border-bottom border-right border-bottom-color border-right-color"
-						dangerouslySetInnerHTML={turn}
-						onClick={() => putMark()}></div>
+						{total_casillas[0]}
+						onClick={() => putMark("0")}></div>
 					<div
 						className="block border-bottom border-bottom-color"
-						dangerouslySetInnerHTML={turn}
-						onClick={() => putMark()}></div>
+						onClick={() => putMark("1")}>
+						{total_casillas[1]}
+					</div>
 					<div
 						className="block border-bottom border-left border-bottom-color border-left-color"
-						dangerouslySetInnerHTML={turn}
-						onClick={() => putMark()}></div>
+						onClick={() => putMark("2")}>
+						{total_casillas[2]}
+					</div>
 				</div>
 				<div className="row justify-content-center">
 					<div
 						className="block border-right border-right-color"
-						dangerouslySetInnerHTML={turn}
-						onClick={() => putMark()}></div>
-					<div
-						className="block"
-						dangerouslySetInnerHTML={turn}
-						onClick={() => putMark()}></div>
+						onClick={() => putMark("3")}>
+						{total_casillas[3]}
+					</div>
+					<div className="block" onClick={() => putMark("4")}>
+						{total_casillas[4]}
+					</div>
 					<div
 						className="block border-left border-left-color"
-						dangerouslySetInnerHTML={turn}
-						onClick={() => putMark()}></div>
+						onClick={() => putMark("5")}>
+						{total_casillas[5]}
+					</div>
 				</div>
 				<div className="row justify-content-center">
 					<div
 						className="block border-top border-right border-top-color border-right-color"
-						dangerouslySetInnerHTML={turn}
-						onClick={() => putMark()}></div>
+						onClick={() => putMark("6")}>
+						{total_casillas[6]}
+					</div>
 					<div
 						className="block border-top border-top-color"
-						dangerouslySetInnerHTML={turn}
-						onClick={() => putMark()}></div>
+						onClick={() => putMark("7")}>
+						{total_casillas[7]}
+					</div>
 					<div
 						className="block border-top border-left border-top-color border-left-color"
-						dangerouslySetInnerHTML={turn}
-						onClick={() => putMark()}></div>
+						onClick={() => putMark("8")}>
+						{total_casillas[8]}
+					</div>
 				</div>
 			</div>
 		</div>
