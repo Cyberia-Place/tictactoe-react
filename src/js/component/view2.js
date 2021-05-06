67c50fb0e6c9e9485b0cb88610f38a54355fdbf0
Cyberia-Place <eliasberto2@gmail.com>
2021-05-06 16:30:34 +0000

    falta el boton de start over

--- src/js/component/view2.js
+++ src/js/component/view2.js
@@ -3,30 +3,51 @@ import PropTypes from "prop-types";
 
 let total_casillas = Array(9).fill("");
 let i = 0;
+
 export const View2 = props => {
+	const evaluateWinner = () => {
+		let ind = 0;
+		let ind2 = 0;
+		for (let iter = 0; iter < 3; iter++) {
+			let fila =
+				total_casillas[0 + ind] +
+				total_casillas[1 + ind] +
+				total_casillas[2 + ind];
+
+			let columna =
+				total_casillas[0 + ind2] +
+				total_casillas[3 + ind2] +
+				total_casillas[6 + ind2];
+
+			if (
+				((fila == "XXX" || fila == "OOO") && fila !== "") ||
+				((columna == "XXX" || columna == "OOO") && columna != "")
+			) {
+				alert(`${turn} Wins`);
+				break;
+			} else {
+				ind += 3;
+				ind2 += 1;
+			}
+		}
+	};
+
 	const [turn, setTurn] = useState(props.player);
-	// console.log(i);
+	let encabezado = `It is ${turn} turn!`;
 	const putMark = casilla => {
 		if (props.player == "X") {
 			let total_turns = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
 			setTurn(total_turns[i]);
 			i++;
 			total_casillas[casilla] = turn;
-			console.log(total_casillas);
 		} else {
 			let total_turns = ["O", "X", "O", "X", "O", "X", "O", "X", "O"];
 			setTurn(total_turns[i]);
 			i++;
 			total_casillas[casilla] = turn;
-			console.log(total_casillas);
 		}
-    };
-    
-    const evaluateWinner = () => {
-        if (total_casillas[0] == total_casillas[1] == total_casillas[2]) {
-            
-        }
-    }
+		evaluateWinner();
+	};
 
 	const tablero = total_casillas.map((item, index) => {
 		return (
@@ -40,7 +61,7 @@ export const View2 = props => {
 
 	return (
 		<div className="container">
-			<h2>It is {turn} Turn!</h2>
+			<h2>{encabezado}</h2>
 			<div className="mx-auto mt-4">
 				<div className="row mx-auto" style={{ width: "60%" }}>
 					{tablero}
